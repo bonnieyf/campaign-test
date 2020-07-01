21ff5441d2a64b108388e43912149ce9ad3169a9
kusasen <kusasen@gmail.com>
2020-07-01 11:50:04 +0800

    update

--- src/templates/ProductPage/index.js
+++ src/templates/ProductPage/index.js
@@ -15,84 +15,84 @@ import {
   ProductDescription
 } from './styles'
 
-const ProductPage = ({ data }) => {
-  const product = data.shopifyProduct
-  return (
-    <>
-      <SEO title={product.title} description={product.description} />
-      <Container>
-        <TwoColumnGrid>
-          <GridLeft>
-            {product.images.map(image => (
-              <Img
-                fluid={image.localFile.childImageSharp.fluid}
-                key={image.id}
-                alt={product.title}
-              />
-            ))}
-          </GridLeft>
-          <GridRight>
-            <ProductTitle>{product.title}</ProductTitle>
-            <ProductDescription
-              dangerouslySetInnerHTML={{ __html: product.descriptionHtml }}
-            />
-            <ProductForm product={product} />
-          </GridRight>
-        </TwoColumnGrid>
-      </Container>
-    </>
-  )
+const ProductPage = () => {
+  // const product = data.shopifyProduct
+  // return (
+  //   <>
+  //     <SEO title={product.title} description={product.description} />
+  //     <Container>
+  //       <TwoColumnGrid>
+  //         <GridLeft>
+  //           {product.images.map(image => (
+  //             <Img
+  //               fluid={image.localFile.childImageSharp.fluid}
+  //               key={image.id}
+  //               alt={product.title}
+  //             />
+  //           ))}
+  //         </GridLeft>
+  //         <GridRight>
+  //           <ProductTitle>{product.title}</ProductTitle>
+  //           <ProductDescription
+  //             dangerouslySetInnerHTML={{ __html: product.descriptionHtml }}
+  //           />
+  //           <ProductForm product={product} />
+  //         </GridRight>
+  //       </TwoColumnGrid>
+  //     </Container>
+  //   </>
+  // )
 }
 
-export const query = graphql`
-  query($handle: String!) {
-    shopifyProduct(handle: { eq: $handle }) {
-      id
-      title
-      handle
-      productType
-      description
-      descriptionHtml
-      shopifyId
-      options {
-        id
-        name
-        values
-      }
-      variants {
-        id
-        title
-        price
-        availableForSale
-        shopifyId
-        selectedOptions {
-          name
-          value
-        }
-      }
-      priceRange {
-        minVariantPrice {
-          amount
-          currencyCode
-        }
-        maxVariantPrice {
-          amount
-          currencyCode
-        }
-      }
-      images {
-        originalSrc
-        id
-        localFile {
-          childImageSharp {
-            fluid(maxWidth: 910) {
-              ...GatsbyImageSharpFluid_withWebp_tracedSVG
-            }
-          }
-        }
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   query($handle: String!) {
+//     shopifyProduct(handle: { eq: $handle }) {
+//       id
+//       title
+//       handle
+//       productType
+//       description
+//       descriptionHtml
+//       shopifyId
+//       options {
+//         id
+//         name
+//         values
+//       }
+//       variants {
+//         id
+//         title
+//         price
+//         availableForSale
+//         shopifyId
+//         selectedOptions {
+//           name
+//           value
+//         }
+//       }
+//       priceRange {
+//         minVariantPrice {
+//           amount
+//           currencyCode
+//         }
+//         maxVariantPrice {
+//           amount
+//           currencyCode
+//         }
+//       }
+//       images {
+//         originalSrc
+//         id
+//         localFile {
+//           childImageSharp {
+//             fluid(maxWidth: 910) {
+//               ...GatsbyImageSharpFluid_withWebp_tracedSVG
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
 
 export default ProductPage
